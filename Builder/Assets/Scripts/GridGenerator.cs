d377c67cfec1a0ac1581479ed9b7d1b79fc3eba4
DESKTOP-FL3NAD3\Sam <s.roach0305@gmail.com>
2018-04-28 14:13:34 +0100

    Grid of cubes generated. No longer using gizmos, there's no need for the update each frame and in editor.

--- Builder/Assets/Scripts/GridGenerator.cs
+++ Builder/Assets/Scripts/GridGenerator.cs
@@ -7,17 +7,19 @@ namespace Builder
     public class GridGenerator : MonoBehaviour
     {
 
-       
+        public GameObject gridSquare; 
         public float gridSize = 20;
         public GameObject plane;
 
         private float planeSize;
         private float gridCountXY;
         private float gizmoDistance;
+        private bool gridGenerate;
 
         public void Awake()
         {
             gizmoDistance = 1f;
+            DrawGrid();
         }
 
         public Vector3 GetNearestPointOnGrid(Vector3 position)
@@ -26,32 +28,45 @@ namespace Builder
             int yCount = Mathf.RoundToInt(position.y / gizmoDistance);
             int zCount = Mathf.RoundToInt(position.z / gizmoDistance);
 
-            Vector3 result = new Vector3((float)xCount * gizmoDistance,(float)yCount * gizmoDistance,(float)zCount * gizmoDistance);
+            Vector3 result = new Vector3((float)xCount * gizmoDistance, (float)yCount * gizmoDistance, (float)zCount * gizmoDistance);
 
             result += transform.position;
 
             return result;
         }
 
-        private void SetPlaneSize()
+        public bool DrawGrid()
         {
-            planeSize = gridSize / 10;
-            plane.transform.localScale += new Vector3(planeSize, 0, planeSize);
-        }
-
-
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = Color.yellow;
             for (float x = 0; x < gridSize; x += gizmoDistance)
             {
                 for (float z = 0; z < gridSize; z += gizmoDistance)
                 {
-                    var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
-                    Gizmos.DrawSphere(point, 0.1f);
+                    Vector3 point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+
+                    Instantiate(gridSquare, new Vector3(point.x, 0, point.z), Quaternion.identity);
                 }
             }
-            SetPlaneSize(); 
+            return (true);
         }
+        //private void SetPlaneSize()
+        //{
+        //    planeSize = gridSize / 10;
+        //    plane.transform.localScale += new Vector3(planeSize, 0, planeSize);
+        //}
+
+
+        //private void OnDrawGizmos()
+        //{
+        //    Gizmos.color = Color.yellow;
+        //    for (float x = 0; x < gridSize; x += gizmoDistance)
+        //    {
+        //        for (float z = 0; z < gridSize; z += gizmoDistance)
+        //        {
+        //            var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+        //            Gizmos.DrawSphere(point, 0.1f);
+        //        }
+        //    }
+            //    SetPlaneSize(); 
+        //}
     }
 }
