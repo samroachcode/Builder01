1fb7380f9377c2e65d2f6911edd24793dad61033
DESKTOP-FL3NAD3\Sam <s.roach0305@gmail.com>
2018-04-28 02:18:34 +0100

    Emergency commit due to PC crashes. Begining of grid based system and scaling using gizmos and plane.

---
+++ Builder/Assets/Scripts/GridGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Builder
+{
+    public class GridGenerator : MonoBehaviour
+    {
+
+       
+        public float gridSize = 20;
+        public GameObject plane;
+
+        private float planeSize;
+        private float gridCountXY;
+        private float gizmoDistance;
+
+        public void Awake()
+        {
+            gizmoDistance = 1f;
+        }
+
+        public Vector3 GetNearestPointOnGrid(Vector3 position)
+        {
+            int xCount = Mathf.RoundToInt(position.x / gizmoDistance);
+            int yCount = Mathf.RoundToInt(position.y / gizmoDistance);
+            int zCount = Mathf.RoundToInt(position.z / gizmoDistance);
+
+            Vector3 result = new Vector3((float)xCount * gizmoDistance,(float)yCount * gizmoDistance,(float)zCount * gizmoDistance);
+
+            result += transform.position;
+
+            return result;
+        }
+
+        private void SetPlaneSize()
+        {
+            planeSize = gridSize / 10;
+            plane.transform.localScale += new Vector3(planeSize, 0, planeSize);
+        }
+
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.yellow;
+            for (float x = 0; x < gridSize; x += gizmoDistance)
+            {
+                for (float z = 0; z < gridSize; z += gizmoDistance)
+                {
+                    var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+                    Gizmos.DrawSphere(point, 0.1f);
+                }
+            }
+            SetPlaneSize(); 
+        }
+    }
+}
