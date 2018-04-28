20c9a5729c5be41d3144db4771b831712ab77cb2
DESKTOP-FL3NAD3\Sam <s.roach0305@gmail.com>
2018-04-28 15:16:09 +0100

    Places grids in hierachy, sets backing plane to scale with grid. Sets grid in center. - Need to scale the cameras distance to fit the plane. - Put the grid locations in an array. List will be less efficient for searching.

--- Builder/Assets/Scripts/GridGenerator.cs
+++ Builder/Assets/Scripts/GridGenerator.cs
@@ -6,67 +6,55 @@ namespace Builder
 {
     public class GridGenerator : MonoBehaviour
     {
-
+        public int gridSize = 20;
+        public float offset = 0.2f; 
         public GameObject gridSquare; 
-        public float gridSize = 20;
         public GameObject plane;
-
+        public GameObject gridParent; 
         private float planeSize;
         private float gridCountXY;
-        private float gizmoDistance;
+        private float gridSpacing;
+
         private bool gridGenerate;
+
+        private Transform[] gridTrans; 
 
         public void Awake()
         {
-            gizmoDistance = 1f;
+            gridSpacing = 1f;
+            SetPlaneSize();
             DrawGrid();
         }
 
         public Vector3 GetNearestPointOnGrid(Vector3 position)
         {
-            int xCount = Mathf.RoundToInt(position.x / gizmoDistance);
-            int yCount = Mathf.RoundToInt(position.y / gizmoDistance);
-            int zCount = Mathf.RoundToInt(position.z / gizmoDistance);
-
-            Vector3 result = new Vector3((float)xCount * gizmoDistance, (float)yCount * gizmoDistance, (float)zCount * gizmoDistance);
-
+            int xCount = Mathf.RoundToInt(position.x / gridSpacing);
+            int yCount = Mathf.RoundToInt(position.y / gridSpacing);
+            int zCount = Mathf.RoundToInt(position.z / gridSpacing);
+            Vector3 result = new Vector3((float)xCount * gridSpacing, (float)yCount * gridSpacing, (float)zCount * gridSpacing);
             result += transform.position;
-
             return result;
         }
 
         public bool DrawGrid()
         {
-            for (float x = 0; x < gridSize; x += gizmoDistance)
+            for (float x = 0; x < gridSize; x += gridSpacing)
             {
-                for (float z = 0; z < gridSize; z += gizmoDistance)
+                for (float z = 0; z < gridSize; z += gridSpacing)
                 {
                     Vector3 point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
-
-                    Instantiate(gridSquare, new Vector3(point.x, 0, point.z), Quaternion.identity);
+                    GameObject go = Instantiate(gridSquare,new Vector3(point.x, 0, point.z), Quaternion.identity);
+                    go.transform.SetParent(gridParent.transform, true);
                 }
             }
+            gridParent.transform.position = new Vector3(-(gridSize+offset)/2,0, -(gridSize + offset) / 2);
             return (true);
         }
-        //private void SetPlaneSize()
-        //{
-        //    planeSize = gridSize / 10;
-        //    plane.transform.localScale += new Vector3(planeSize, 0, planeSize);
-        //}
 
-
-        //private void OnDrawGizmos()
-        //{
-        //    Gizmos.color = Color.yellow;
-        //    for (float x = 0; x < gridSize; x += gizmoDistance)
-        //    {
-        //        for (float z = 0; z < gridSize; z += gizmoDistance)
-        //        {
-        //            var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
-        //            Gizmos.DrawSphere(point, 0.1f);
-        //        }
-        //    }
-            //    SetPlaneSize(); 
-        //}
+        private void SetPlaneSize()
+        {
+            planeSize = gridSize / 10;
+            plane.transform.localScale += new Vector3(planeSize+offset, 0, planeSize+offset);
+        }
     }
 }
