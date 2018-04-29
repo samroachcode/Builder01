06d4c99f1260bf767dfec69c58aac76c560c6b22
DESKTOP-FL3NAD3\Sam <s.roach0305@gmail.com>
2018-04-29 17:58:19 +0100

    Input tap and drag implemented. Decouple input and movement of buildings. Add tap and drag as seperate actions.

--- Builder/Assets/Scripts/InputManager.cs
+++ Builder/Assets/Scripts/InputManager.cs
@@ -7,26 +7,42 @@ namespace Builder
     public class InputManager : MonoBehaviour
     {
         private bool userInput;
-
+        private GameObject go;
+        //private float inputTimer; 
         private void Awake()
         {
+            go = null; 
             userInput = false;
+            inputTimer = 0; 
         }
 
-        private void Update()
+        private void Update() // add taps and drags. Not worth doing an event system for this style 
         {
             if (Input.GetMouseButton(0))
             {
+
                 RaycastToBuildings(Input.mousePosition);
                 userInput = true;
             }
-            if(Input.touchCount > 0)
+            if (Input.touchCount > 0)
             {
                 RaycastToBuildings(Input.GetTouch(1).position);
                 userInput = true;
             }
-            else
+            if (!Input.GetMouseButton(0))
+            {
                 userInput = false;
+                go = null;
+            }
+            if (userInput && go != null)
+            {
+                Vector3 inputWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,29)) ;
+                
+                go.transform.position = inputWorldPos;
+            }
+            else if (!userInput)
+                go = null; 
+            
         }
 
         private void RaycastToBuildings(Vector3 position)
@@ -35,12 +51,13 @@ namespace Builder
             Ray ray = Camera.main.ScreenPointToRay(position);
             if(Physics.Raycast(ray, out hit))
             {
-                if (hit.transform.tag == "Building")
+                if (hit.transform.tag == "Building" && go == null)
                 {
-                    Debug.Log("DRAG");
-                    //hit.transform.position = new Vector3(ray.x, 0, ray.z);
+                    go = hit.transform.gameObject;
                 }
             }
         }
+
+
     }
 }
