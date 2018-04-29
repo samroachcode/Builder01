7c055dfdd44dd15e8fdc2f2dbbb7f8ac9db0ead8
DESKTOP-FL3NAD3\Sam <s.roach0305@gmail.com>
2018-04-29 13:32:34 +0100

    Input Manager created to manage mouse and touch inputs.

---
+++ Builder/Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Builder
+{
+    public class InputManager : MonoBehaviour
+    {
+        private bool userInput;
+
+        private void Awake()
+        {
+            userInput = false;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                RaycastToBuildings(Input.mousePosition);
+                userInput = true;
+            }
+            if(Input.touchCount > 0)
+            {
+                RaycastToBuildings(Input.GetTouch(1).position);
+                userInput = true;
+            }
+            else
+                userInput = false;
+        }
+
+        private void RaycastToBuildings(Vector3 position)
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(position);
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.tag == "Building")
+                {
+                    Debug.Log("DRAG");
+                    //hit.transform.position = new Vector3(ray.x, 0, ray.z);
+                }
+            }
+        }
+    }
+}
